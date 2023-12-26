da899a3ff1e0b7d5429d3b97f7e8424b008467d6
matite <matiastite1@gmail.com>
2023-12-26 12:11:29 -0500

    Corrección formularios

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -10,6 +10,9 @@ from .models import (
     Processor,
     Disk,
     Ram,
+    Air_Conditioner,
+    Regulator_voltage,
+    Security_camera,
 )
 
 # Register your models here.
@@ -23,3 +26,6 @@ admin.site.register(Task)
 admin.site.register(Processor)
 admin.site.register(Disk)
 admin.site.register(Ram)
+admin.site.register(Air_Conditioner)
+admin.site.register(Regulator_voltage)
+admin.site.register(Security_camera)
