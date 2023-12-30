0d642f5ef3e2dc5ea8677144a42d2b3f3749c334
Edder12 <enaranjo5489@uta.edu.ec>
2023-12-30 15:30:20 -0500

    Agregado register.html y forms.py dentro de users

---
+++ apps/users/forms.py
@@ -0,0 +1,10 @@
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
+
+User = get_user_model()
+
+class UserCreationForm(DjangoUserCreationForm):
+    class Meta(DjangoUserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email') 
