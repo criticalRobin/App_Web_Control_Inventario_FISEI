eb81442ac278b7c4b819f3dec04a28a294755cfe
Edder12 <enaranjo5489@uta.edu.ec>
2023-12-30 15:32:02 -0500

    Actualizado logica de funcionamiento para el register del usuario

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from apps.users.views import dashboard_view  # new
+from .views import dashboard_view, register  # Importa la vista de registro
 
 
 app_name = 'users'  
 
 urlpatterns = [
     path("dashboard/", dashboard_view, name="dashboard"),
+    path("register/", register, name="register"),  # Agrega la URL de registro
+
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,  authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import UserCreationForm
 
-# Create your views here.
+
 @login_required
 def dashboard_view(request):
+    # Tu lógica aquí
     return render(request, 'dashboard.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('users:dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
