acc8818ccf03eecdd69e099bbce52b9e6dd3b2ed
Edder12 <enaranjo5489@uta.edu.ec>
2023-12-30 18:44:49 -0500

    Implementado logica para el registro de usuarios

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,10 +1,32 @@
-
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 User = get_user_model()
 
-class UserCreationForm(DjangoUserCreationForm):
-    class Meta(DjangoUserCreationForm.Meta):
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Correo Electrónico")
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmación de Contraseña", widget=forms.PasswordInput)
+    
+    username = forms.CharField(
+        help_text='',  
+         )
+
+    class Meta:
         model = User
-        fields = ('username', 'email') 
+        fields = ("username", "email", "password1", "password2")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coinciden")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login,  authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import UserCreationForm
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 
 @login_required
 def dashboard_view(request):
-    # Tu lógica aquí
     return render(request, 'dashboard.html')
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('users:dashboard')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
