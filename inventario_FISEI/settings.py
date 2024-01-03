2b35c9ca05e5c078a6f0b9004af6973b47e5cfa3
Henry Cortez <henrystevencortez@yahoo.es>
2024-01-03 10:14:19 -0500

    Se traspasa las templetes y se edita la ruta donde se encuentran.

--- inventario_FISEI/settings.py
+++ inventario_FISEI/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = "inventario_FISEI.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR / "home/templates")],
+        "DIRS": [os.path.join(BASE_DIR / "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
