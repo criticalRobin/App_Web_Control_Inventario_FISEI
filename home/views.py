25ca458f9fa39b1ddf83483eaa6403c2d8f7bc07
Henry Cortez <henrystevencortez@yahoo.es>
2024-01-06 22:24:54 -0500

    Se hizo que cargue el argon desde la app de home

--- home/views.py
+++ home/views.py
@@ -3,8 +3,8 @@ from django.db.models import Count
 from django.db.models.functions import TruncMonth
 from apps.users.models import User
 from apps.main.models import Laboratory, Computer, Recommendation, Task, LabItem, ComputerItem
-
-
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView,PasswordChangeView
+from .forms import RegistrationForm, LoginForm, UserPasswordResetForm, UserSetPasswordForm, UserPasswordChangeForm
 # Create your views here.
 def dashboard_stats_view(request):
     # Aquí puedes agregar la lógica para calcular las estadísticas de tus modelos.
@@ -74,3 +74,63 @@ def dashboard_stats_view(request):
 
     # No renderizamos la plantilla aquí, solo devolvemos el contexto
     return context
+
+def argon_dashboard_view(request):
+    context = dashboard_stats_view(request)
+    return render(request, 'pages/dashboard.html', context)
+
+def index(request):
+  return render(request, 'pages/dashboard.html')
+
+def billing(request):
+  return render(request, 'pages/billing.html')
+
+def profile(request):
+  return render(request, 'pages/profile.html')
+
+def tables(request):
+  return render(request, 'pages/tables.html')
+
+def rtl(request):
+  return render(request, 'pages/rtl.html')
+
+def vr(request):
+  return render(request, 'pages/virtual-reality.html')
+
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print("Account created successfully!")
+      return redirect('/accounts/login/')
+    else:
+      print("Registration failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/sign-up.html', context)
+
+
+class UserLoginView(LoginView):
+  template_name = 'accounts/sign-in.html'
+  form_class = LoginForm
+
+
+class UserPasswordResetView(PasswordResetView):
+  template_name = 'accounts/password_reset.html'
+  form_class = UserPasswordResetForm
+
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+  template_name = 'accounts/password_reset_confirm.html'
+  form_class = UserSetPasswordForm
+
+class UserPasswordChangeView(PasswordChangeView):
+  template_name = 'accounts/password_change.html'
+  form_class = UserPasswordChangeForm
+
+def user_logout_view(request):
+  logout(request)
+  return redirect('/accounts/login/')
