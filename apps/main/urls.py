b7932d6c869e21425fcc10faa907567449647fdc
matite <matiastite1@gmail.com>
2024-01-07 18:08:44 -0500

    Implementado listado items por laboratorio

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,11 +3,13 @@ from apps.main.views.computers.views import ComputerCreateView
 from apps.main.views.labs.views import LaboratoryCreateView, LaboratoryListView
 from apps.main.views.recomms.views import RecommendationCreateView
 from apps.main.views.tasks.views import TaskCreateView
-
+from apps.main.views.items.views import LabItemListView
 
 
 app_name = "main"
 
 urlpatterns = [
-    path('labs/', LaboratoryListView.as_view(), name='labs_list'),
+    # LIST URLS
+    path("labs/", LaboratoryListView.as_view(), name="labs_list"),
+    path("labs/<int:pk>/items/", LabItemListView.as_view(), name="items_list"),
 ]
--- apps/main/views/items/views.py
+++ apps/main/views/items/views.py
@@ -1,16 +1,20 @@
-from apps.main.models import LabItem
+from apps.main.models import LabItem, Laboratory
 from django.views.generic import ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from home.views import dashboard_stats_view
 
 
-class LaboratoryListView(ListView):
+class LabItemListView(ListView):
     model = LabItem
     template_name = "items/list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        lab = self.kwargs["pk"]
+        laboratory = Laboratory.objects.get(id=lab)
+        lab_items = LabItem.objects.filter(id_laboratory=laboratory)
+        context["lab_items"] = lab_items
         dashboard_context = dashboard_stats_view(self.request)
         context.update(dashboard_context)
         return context
